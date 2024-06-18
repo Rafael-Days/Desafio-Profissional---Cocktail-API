363ae4d389749214bdae0ac6a8e579c10184e13e
Rafael Days <130679216+Rafael-Days@users.noreply.github.com>
2024-06-18 13:12:22 +0000

    att

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+/*import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://0.0.0.0/books-nest'),
+    BooksModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}*/
